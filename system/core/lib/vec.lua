ff415e5bf884e535553e046f98815690faea97cc
igorkll <frerther@gmail.com>
2023-10-12 19:49:21 +0300

    Update vec.lua

--- system/core/lib/vec.lua
+++ system/core/lib/vec.lua
@@ -4,49 +4,76 @@ local vec = {}
 
 vec.meta3 = {
     __add = function (a, b)
+        if type(a) == "number" then a = vec.vec3(a, a, a) end
+        if type(b) == "number" then b = vec.vec3(b, b, b) end
         return vec.vec3(a.x + b.x, a.y + b.y, a.z + b.z)
     end,
     __sub = function (a, b)
+        if type(a) == "number" then a = vec.vec3(a, a, a) end
+        if type(b) == "number" then b = vec.vec3(b, b, b) end
         return vec.vec3(a.x - b.x, a.y - b.y, a.z - b.z)
     end,
     __mul = function (a, b)
+        if type(a) == "number" then a = vec.vec3(a, a, a) end
+        if type(b) == "number" then b = vec.vec3(b, b, b) end
         return vec.vec3(a.x * b.x, a.y * b.y, a.z * b.z)
     end,
     __div = function (a, b)
+        if type(a) == "number" then a = vec.vec3(a, a, a) end
+        if type(b) == "number" then b = vec.vec3(b, b, b) end
         return vec.vec3(a.x / b.x, a.y / b.y, a.z / b.z)
     end,
     __eq = function (a, b)
         return a.x == b.x and a.y == b.y and a.z == b.z
     end,
     __lt = function (a, b)
-        return a:len() < b:len()
+        local alen = type(a) == "number" and a or a:len()
+        local blen = type(b) == "number" and b or b:len()
+        return alen < blen
     end,
     __le = function (a, b)
-        return a:len() <= b:len()
+        local alen = type(a) == "number" and a or a:len()
+        local blen = type(b) == "number" and b or b:len()
+        return alen <= blen
+    end,
+    __len = function (vec)
+        return vec:len()
     end
 }
 
 vec.meta2 = {
     __add = function (a, b)
+        if type(a) == "number" then a = vec.vec2(a, a) end
+        if type(b) == "number" then b = vec.vec2(b, b) end
         return vec.vec2(a.x + b.x, a.y + b.y)
     end,
     __sub = function (a, b)
+        if type(a) == "number" then a = vec.vec2(a, a) end
+        if type(b) == "number" then b = vec.vec2(b, b) end
         return vec.vec2(a.x - b.x, a.y - b.y)
     end,
     __mul = function (a, b)
+        if type(a) == "number" then a = vec.vec2(a, a) end
+        if type(b) == "number" then b = vec.vec2(b, b) end
         return vec.vec2(a.x * b.x, a.y * b.y)
     end,
     __div = function (a, b)
+        if type(a) == "number" then a = vec.vec2(a, a) end
+        if type(b) == "number" then b = vec.vec2(b, b) end
         return vec.vec2(a.x / b.x, a.y / b.y)
     end,
     __eq = function (a, b)
         return a.x == b.x and a.y == b.y
     end,
     __lt = function (a, b)
-        return a:len() < b:len()
+        local alen = type(a) == "number" and a or a:len()
+        local blen = type(b) == "number" and b or b:len()
+        return alen < blen
     end,
     __le = function (a, b)
-        return a:len() <= b:len()
+        local alen = type(a) == "number" and a or a:len()
+        local blen = type(b) == "number" and b or b:len()
+        return alen <= blen
     end,
     __len = function (vec)
         return vec:len()
