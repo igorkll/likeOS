37fa26718cfa069c3308e905f4cc54bc18b2a95d
igorkll <frerther@gmail.com>
2023-10-18 21:29:17 +0300

    Update cache.lua

--- system/core/lib/cache.lua
+++ system/core/lib/cache.lua
@@ -1,6 +1,6 @@
 local cache = {}
 cache.cache = {}
-cache.copiedText = "PASTE" --put the copied text here
+cache.copiedText = nil --put the copied text here
 
 function cache.clearCache()
     for key, value in pairs(cache.cache) do
