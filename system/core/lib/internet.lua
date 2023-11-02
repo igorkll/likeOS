f7a5d2819f536df647670b85bb2bf4e0320f82a0
igorkll <frerther@gmail.com>
2023-11-02 22:58:48 +0300

    added internet.download method

--- system/core/lib/internet.lua
+++ system/core/lib/internet.lua
@@ -1,7 +1,8 @@
 local component = require("component")
+local fs = require("filesystem")
 local internet = {}
 
-function internet.getInternetFile(url)
+function internet.get(url)
     local inet = component.proxy(component.list("internet")() or "")
 
     if not inet then
@@ -32,5 +33,40 @@ function internet.getInternetFile(url)
     end
 end
 
+function internet.download(url, path)
+    local inet = component.proxy(component.list("internet")() or "")
+
+    if not inet then
+        return nil, "no internet-card"
+    end
+
+    local handle = inet.request(url)
+    if handle then
+        local file, err = fs.open(path, "wb")
+        if not file then
+            return nil, err
+        end
+        
+        while true do
+            local result, reason = handle.read(math.huge) 
+            if result then
+                file.write(result)
+            else
+                file.close()
+                handle.close()
+                
+                if reason then
+                    return nil, reason
+                else
+                    return true
+                end
+            end
+        end
+    else
+        return nil, "invalid address"
+    end
+end
+
+internet.getInternetFile = internet.get
 internet.unloadable = true
 return internet
