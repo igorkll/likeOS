4e810129c845ee48e3ef26a6b2e5812842fccfcb
igorkll <frerther@gmail.com>
2023-11-16 18:41:09 +0300

    new cache update template

--- system/core/lib/cache.lua
+++ system/core/lib/cache.lua
@@ -1,6 +1,24 @@
+local fs = require("filesystem")
+local paths = require("paths")
+
+local runtimeCache = "/data/cache/runtime"
+fs.remove(runtimeCache)
+fs.makeDirectory(runtimeCache)
+
+local constCache = "/data/cache/const"
+fs.makeDirectory(constCache)
+
+
+local function createRuntimeCache(folder)
+    
+end
+
+
 local cache = {}
 cache.cache = {} --can be cleaned at any time
 cache.data = {} --in the future it will be saved to the hard disk
+cache.runtime = {} --immediately saved to the hard disk(cleared after reboot)
+cache.const = {} --instantly saved to the hard disk (not deleted)
 
 function cache.clearCache()
     for key, value in pairs(cache.cache) do
