b5110521daae6e49420e596a04fb4548ba0e088b
igorkll <frerther@gmail.com>
2024-01-02 21:37:16 +0300

    Update vcursor.lua

--- system/core/lib/vcursor.lua
+++ system/core/lib/vcursor.lua
@@ -13,6 +13,7 @@ function vcursor.hook(screen)
     local active = false
     local pressed1 = false
     local pressed2 = false
+    local invertFlag = false
     local posx, posy = 1, 1
 
     local function invertPos(x, y)
@@ -29,14 +30,20 @@ function vcursor.hook(screen)
 
         return utils.safeExec(function ()
             if not enabled[screen] then
-                if active then
+                if active and not invertFlag then
                     invertPos()
                 end
                 active = false
+                invertFlag = false
                 return
             end
 
             if (eventData[1] == "key_down" or eventData[1] == "key_up") and table.exists(lastinfo.keyboards[screen], eventData[2]) then
+                if eventData[1] == "key_up" and invertFlag then
+                    invertPos()
+                    invertFlag = false
+                end
+
                 if eventData[3] == 0 and eventData[4] == 56 then
                     if eventData[1] == "key_down" then
                         active = true
@@ -53,44 +60,54 @@ function vcursor.hook(screen)
                     if eventData[1] == "key_down" then
                         local rx, ry = graphic.getResolution(screen)
                         local newposx, newposy = posx, posy
-                        local cursorMove, cursorAction = false, false
+                        local cursorMove, cursorAction, replaceEvent = false, false, false
 
                         if eventData[4] == 17 then
                             cursorMove = true
+                            replaceEvent = true
                             newposy = newposy - 1
                             if newposy < 1 then newposy = 1 end
                         elseif eventData[4] == 31 then
                             cursorMove = true
+                            replaceEvent = true
                             newposy = newposy + 1
                             if newposy > ry then newposy = ry end
                         elseif eventData[4] == 30 then
                             cursorMove = true
+                            replaceEvent = true
                             newposx = newposx - 1
                             if newposx < 1 then newposx = 1 end
                         elseif eventData[4] == 32 then
                             cursorMove = true
+                            replaceEvent = true
                             newposx = newposx + 1
                             if newposx > rx then newposx = rx end
                         elseif eventData[4] == 16 then
-                            cursorAction = true
+                            replaceEvent = true
                             if not pressed1 then
                                 event.push("touch", screen, posx, posy, 0, eventData[5])
+                                cursorAction = true
                             end
                             pressed1 = true
                         elseif eventData[4] == 18 then
-                            cursorAction = true
+                            replaceEvent = true
                             if not pressed2 then
                                 event.push("touch", screen, posx, posy, 1, eventData[5])
+                                cursorAction = true
                             end
                             pressed2 = true
                         end
 
                         if newposx ~= posx or newposy ~= posy then
-                            invertPos()
+                            if not invertFlag then
+                                invertPos()
+                            end
                             posx, posy = newposx, newposy
                             invertPos()
 
                             if cursorMove then
+                                invertFlag = false
+
                                 if pressed1 then
                                     event.push("drag", screen, posx, posy, 0, eventData[5])
                                 end
@@ -101,7 +118,12 @@ function vcursor.hook(screen)
                             end
                         end
 
-                        if cursorMove or cursorAction then
+                        if cursorAction then
+                            invertPos()
+                            invertFlag = true
+                        end
+
+                        if replaceEvent then
                             eventData[1] = "vcursor_" .. eventData[1]
                         end
                     elseif eventData[1] == "key_up" then
