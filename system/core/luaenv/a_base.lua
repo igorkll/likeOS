fbbf4aa2034dc99f71971a464983ca20dea2a53e
igorkll <frerther@gmail.com>
2024-01-10 14:40:22 +0300

    Update a_base.lua

--- system/core/luaenv/a_base.lua
+++ system/core/luaenv/a_base.lua
@@ -2,7 +2,7 @@ function _G.loadfile(path, mode, env)
     local fs = require("filesystem")
     local content, err = fs.readFile(path)
     if not content then return nil, err end
-    return load(content, "=" .. path, mode, env)
+    return load(content, "=" .. path, mode, env or require("bootloader").createEnv())
 end
 
 --вы не должны запускать им проги! это вызовет проблеммы с обработкой ошибок и потоками в целевой программе, для запуска программ используйте programs.execute
