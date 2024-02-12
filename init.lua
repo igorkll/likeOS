b084756691d31d3a0178c60c9b95c4eb9836b00c
igorkll <frerther@gmail.com>
2024-02-12 18:13:42 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -22,6 +22,16 @@ local function loadfile(fs, path, mode, env)
     return load(data, "=" .. path, mode or "bt", env or _G)
 end
 
+local function unserialize(str)
+    local code = load("return " .. str, "=unserialize", "t", {math={huge=math.huge}})
+    if code then
+        local result = {pcall(code)}
+        if result[1] and type(result[2]) == "table" then
+            return result[2]
+        end
+    end
+end
+
 -------------------------------------------- launch the bootmanager (if any)
 
 local bootmanagerfile = "/bootmanager/main.lua"
@@ -33,12 +43,14 @@ end
 
 local bootfile = "/system/core/bootloader.lua"
 local bootproxy = bootfs
+local bootargs = {}
 
 --------------------------------------------
 
 local bootloaderSettingsPath = "/bootloader"
 local bootloaderSettingsPath_bootfile = bootloaderSettingsPath .. "/bootfile"
 local bootloaderSettingsPath_bootaddr = bootloaderSettingsPath .. "/bootaddr"
+local bootloaderSettingsPath_bootargs = bootloaderSettingsPath .. "/bootargs"
 
 --------------------------------------------
 
@@ -54,12 +66,16 @@ if tmpfs.exists(bootloaderSettingsPath_bootaddr) then
     bootproxy = assert(component.proxy(bootaddr))
 end
 
+if tmpfs.exists(bootloaderSettingsPath_bootargs) then
+    bootargs = unserialize(assert(readFile(tmpfs, bootloaderSettingsPath_bootargs)))
+end
+
 tmpfs.remove(bootloaderSettingsPath)
 
 --------------------------------------------
 
 if bootproxy.exists(bootfile) then
-    assert(load(assert(readFile(bootproxy, bootfile)), "=" .. bootfile, nil, _ENV))()
+    assert(load(assert(readFile(bootproxy, bootfile)), "=" .. bootfile, nil, _ENV))(table.unpack(bootargs))
 else
     local lowLevelInitializer = "/likeOS_startup.lua" --может использоваться для запуска обновления системы
     if bootproxy.exists(lowLevelInitializer) then
